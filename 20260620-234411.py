89d6252e30e65a4798b2677833c433fdf1d12fe8
Andre@ Torres <andreapotato17@gmail.com>
2026-06-20 23:48:15 -0500

    Add files via upload
    Some python files for practice I just try to copy some of my projects of c++ in python, I also have in lua, actually I don't know why I stop doing python, watching it now looks pretty funny, maybe when i got a better pc try again this

---
+++ 20260620-234411.py
@@ -0,0 +1,6 @@
+N = int(input("Programa para saber si es par o impar "))
+
+if N % 2 == 0 :
+    print("Es par")
+else:
+    print("es impar")   
---
+++ 20260620-234441.py
@@ -0,0 +1,6 @@
+E = int(input("Introduce tu edad for empanada: "))
+
+if E < 7:
+    print("no puedes comer empanada")
+else:
+    print("puedes comer empanada")
